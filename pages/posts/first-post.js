832901d72e76526a92d3f1a5d4e4d8110d02ca5c
adarsh <adarsh.chimnani@gmail.com>
2026-01-05 23:38:53 +0100

    Layout component imported in firstpost.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Layout from "../../layout";
 
 export default function FirstPost() {
-    <>
+    <Layout>
         <h1>First Post</h1>
         <h2>
             <Link href='/'>Back to home</Link>
     </h2 >
-     </>    
+     </Layout>    
 }
