118fc4fbc789379f887a3e2e5fdb90eab62881f8
adarsh <adarsh.chimnani@gmail.com>
2026-01-06 23:45:25 +0100

    Minor change

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,8 @@
 import styles from './layout.module.css';
 
 export default function Layout({children}){
-    return <div className={styles.container}>{children}</div>;
+    return <div className={styles.container}>
+        {children}
+        <h1>This is Layout Component</h1>
+    </div>;
 }
