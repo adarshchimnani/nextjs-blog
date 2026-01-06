8f4b66bab0913158766bac65770dc4ba95c16fda
adarsh <adarsh.chimnani@gmail.com>
2026-01-06 23:49:14 +0100

    Minor change

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -11,5 +11,6 @@ export default function FirstPost() {
             <Link href='/'>Back to home</Link>
         </h2 >
         <h3>This is second post.</h3>
+        <h4>This is third post.</h4>
     </Layout>
 }
