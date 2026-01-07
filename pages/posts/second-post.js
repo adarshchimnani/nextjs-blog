c3cece037af35de7383b9d32ed58f7207c407d04
adarsh <adarsh.chimnani@gmail.com>
2026-01-07 23:53:23 +0100

    second-post.js created

---
+++ pages/posts/second-post.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Layout from "../../components/layout";
+import Head from "next/head";
+
+export default function SecondPost() {
+    <Layout>
+        <Head>
+            <title>Second Post</title>
+        </Head>
+        <h2>
+            <Link href='/'>Back to home</Link>
+        </h2 >
+      
+    </Layout>
+}
